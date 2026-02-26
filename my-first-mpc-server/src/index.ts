d2fb9b146a2222775fa0799b5a50bd75e5fb5b4e
니꼬 <nico@nomadcoders.co>
2026-02-26 18:03:36 +0100

    1.2 Your First MCP Server

--- my-first-mpc-server/src/index.ts
+++ my-first-mpc-server/src/index.ts
@@ -1,5 +1,34 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import z from 'zod';
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const server = new McpServer({
+			name: 'Stocks Server',
+			version: '1.0',
+		});
+
+		server.registerTool(
+			'get-stock-price',
+			{
+				description: 'Get the price of a stock given a ticker symbol.',
+				inputSchema: {
+					symbol: z.string(),
+				},
+				// { symbol: "AAPL" }
+			},
+			async ({ symbol }) => {
+				return {
+					content: [
+						{
+							type: 'text',
+							text: `The price of ${symbol} is $10 USD.`,
+						},
+					],
+				};
+			},
+		);
+
 		return new Response('Bye bye World!');
 	},
 } satisfies ExportedHandler<Env>;
