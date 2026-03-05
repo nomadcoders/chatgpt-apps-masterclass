0344632e00030e09cf431c85a1fbf14407662db1
니꼬 <nico@nomadcoders.co>
2026-03-05 17:52:54 +0100

    3.5 Apps SDK UI

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 const FULL_URL = "https://server.nomadcoders.workers.dev/";
@@ -6,7 +7,7 @@ const FULL_URL = "https://server.nomadcoders.workers.dev/";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: mode === "production" ? FULL_URL : undefined,
     build: {
       outDir: "../server/dist",
