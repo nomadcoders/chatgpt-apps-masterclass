bebc7c0368568b53a129ff1fe6e438a62a4de7de
니꼬 <nico@nomadcoders.co>
2026-03-05 16:59:17 +0100

    3.3 Deployment Pipeline

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const FULL_URL = "https://server.nomadcoders.workers.dev/";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../server/dist",
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? FULL_URL : undefined,
+    build: {
+      outDir: "../server/dist",
+      emptyOutDir: true,
+    },
+  };
 });
