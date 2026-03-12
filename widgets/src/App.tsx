48315a6827b499677740ea605c4c32a3eb9a611c
니꼬 <nico@nomadcoders.co>
2026-03-12 16:59:14 +0100

    5.3 Create Deck

--- widgets/src/App.tsx
+++ widgets/src/App.tsx
@@ -1,20 +1,12 @@
 import { useApp, useHostStyles } from "@modelcontextprotocol/ext-apps/react";
 import { LoadingIndicator } from "@openai/apps-sdk-ui/components/Indicator";
 import { useState } from "react";
-import type { MovieDetail, MoviesResponse } from "./types";
-import { MoviesList } from "./movie-list";
-import { MovieDetails } from "./movie-details";
-
-interface ToolOutput {
-  movies?: MoviesResponse;
-  movie?: MovieDetail;
-}
 
 function App() {
-  const [toolOutput, setToolOutput] = useState<ToolOutput | null>(null);
+  const [toolOutput, setToolOutput] = useState<object | null>(null);
 
   const { app } = useApp({
-    appInfo: { name: "Movies Client", version: "1.0" },
+    appInfo: { name: "Flashcards Client", version: "1.0" },
     capabilities: {},
     onAppCreated: (app) => {
       app.ontoolresult = (result) => {
@@ -25,17 +17,9 @@ function App() {
     },
   });
 
-  console.log(app?.getHostContext());
-
   useHostStyles(app, app?.getHostContext());
 
-  if (toolOutput?.movies) {
-    return <MoviesList movies={toolOutput.movies} />;
-  }
-
-  if (toolOutput?.movie) {
-    return <MovieDetails movie={toolOutput.movie} />;
-  }
+  console.log(toolOutput);
 
   return (
     <div className="items-center justify-center flex min-h-50">
